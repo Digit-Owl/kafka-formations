7f9cee65eb45e19765f01e822f732772a26d4baf
jocelyn.pichonnat <jocelyn.pichonnat@gmail.com>
2022-08-24 15:47:19 -0400

    An example of count Kstreams

---
+++ OwlKStreamsStateful/src/main/java/org/lafabriquedigitowl/OwlKStreamsStatefulApplication.java
@@ -0,0 +1,13 @@
+package org.lafabriquedigitowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlKStreamsStatefulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwlKStreamsStatefulApplication.class, args);
+    }
+
+}
